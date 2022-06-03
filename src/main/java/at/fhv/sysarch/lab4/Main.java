d5864775592a901623015e468bbd93530617da6a
Jeremias HEEL <150070@htlr.onmicrosoft.com>
2022-06-03 08:07:53 +0200

    Initial commit

---
+++ src/main/java/at/fhv/sysarch/lab4/Main.java
@@ -0,0 +1,9 @@
+package at.fhv.sysarch.lab4;
+
+import javafx.application.Application;
+
+public class Main {
+    public static void main(String[] args) {
+        Application.launch(MainApplication.class);
+    }
+}
